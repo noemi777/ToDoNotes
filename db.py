72627063420364ab7e416f315886943568c5095f
noemi777 <ada_hack505@womenintech.mx>
2024-03-09 15:20:41 -0600

    Conexión para postgres

--- db.py
+++ db.py
@@ -6,15 +6,22 @@ from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-class Notes(SQLModel, table=True):
+"""class Notes(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     title: str = Field(index=True)
-    description: str
+    description: str"""
     
 
-sqlite_url = "sqlite:///db.sqlite"
+URL_DATABASE = 'postgresql://postgres:Lr35ui+)@localhost:5432/todonotesdn'
+
+engine = create_engine(URL_DATABASE)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+#sqlite_url = "sqlite:///db.sqlite"
 #postgres_url= "postgresql://user:password@postgresserver/db"
 #create engine
-engine = create_engine(sqlite_url, echo=True)
+#engine = create_engine(sqlite_url, echo=True)
 
-SQLModel.metadata.create_all(engine)
+#SQLModel.metadata.create_all(engine)
