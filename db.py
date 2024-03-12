39413d8f121a9e8c33783d226a9d16abbb3c6b71
noemi777 <ada_hack505@womenintech.mx>
2024-03-11 22:37:00 -0600

    Conexión db postgres

--- db.py
+++ db.py
@@ -1,27 +1,22 @@
-from datetime import datetime
-from typing import Optional, Union
-from pydantic import BaseModel
 from sqlmodel import *
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
+from pathlib import Path
+from dotenv import load_dotenv
 
-"""class Notes(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    title: str = Field(index=True)
-    description: str"""
-    
-
-URL_DATABASE = 'postgresql://postgres:Lr35ui+)@localhost:5432/todonotesdn'
-
+env_path = Path(".")/".env"
+load_dotenv(dotenv_path=env_path)
+URL_DATABASE = str = os.getenv('URL_DATABASE')
 engine = create_engine(URL_DATABASE)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-#sqlite_url = "sqlite:///db.sqlite"
-#postgres_url= "postgresql://user:password@postgresserver/db"
-#create engine
-#engine = create_engine(sqlite_url, echo=True)
 
-#SQLModel.metadata.create_all(engine)
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
