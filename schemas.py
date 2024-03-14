05e5cbd780944db12d8c765602165970ab2ceeff
noemi777 <ada_hack505@womenintech.mx>
2024-03-14 00:01:14 -0600

    File schemas

---
+++ schemas.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel,  EmailStr
+from datetime import datetime
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+class TokenData(BaseModel):
+    email: str 
+    scopes: list[str] = []
+
+class NotesBase(BaseModel):
+    title : str 
+    description : str
+
+class Notes(NotesBase):
+    id: int
+    user_id : int
+
+class UserBase(BaseModel):
+    email: EmailStr
+    nickname: str
+    full_name:str
+    registered_at : datetime
+    updated_at : datetime
+    notes: list[Notes] = []
+
+class UserCreate(UserBase):
+    hashed_password: str
