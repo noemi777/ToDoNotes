4d0dda045cacfba5a786c4c74183addc04a5b36c
noemi777 <ada_hack505@womenintech.mx>
2024-03-14 00:00:10 -0600

    Actualización de variables de .env

--- db.py
+++ db.py
@@ -1,13 +1,17 @@
 from sqlmodel import *
-from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 from pathlib import Path
 from dotenv import load_dotenv
 
+#Enviroment
 env_path = Path(".")/".env"
 load_dotenv(dotenv_path=env_path)
+
+#Access DB
 URL_DATABASE = str = os.getenv('URL_DATABASE')
+print(URL_DATABASE)
 engine = create_engine(URL_DATABASE)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -20,3 +24,7 @@ def get_db():
         yield db
     finally:
         db.close()
+
+#Others access
+SECRET_KEY = str = os.getenv('SECRET_KEY')
+ALGORITHM = str = os.getenv('ALGORITHM')
